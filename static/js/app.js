4d9b01d48033bfd925388cf418d9ececffe2cddf
nika9916 <nikoloz.dalakishvili.1@iliauni.edu.ge>
2023-03-10 17:19:09 +0400

    1

---
+++ static/js/app.js
@@ -0,0 +1,8 @@
+const btnLoad = document.getElementById("btn")
+const card1 = document.querySelector(".card1")
+
+
+btnLoad.addEventListener("click", function () {
+    card1.style.display = "block";
+});
+
